92d6cc9d13f5c95487a6c45561585a0794d95e84
scagender <jpverafortune@uc.cl>
2024-04-02 22:32:35 -0300

    invalid name

--- CodigoBaseT1/CodigoBase/core/rules/invalid_name.py
+++ CodigoBaseT1/CodigoBase/core/rules/invalid_name.py
@@ -1,4 +1,5 @@
 from ..rule import *
+import ast
 
 # Clases que permiten detectar el uso de un nombre invalido en clases, metodos y funciones
 
@@ -6,11 +7,38 @@ class InvalidNameVisitor(WarningNodeVisitor):
 
     def __init__(self):
         super().__init__()
+        self.warnings = []
+        self.current_class = None  # Mantener un seguimiento de la clase actual
+
+    def visit_ClassDef(self, node):
+        self.current_class = node  # Establecer la clase actual
+        # Verificar el nombre de la clase
+        class_name = node.name
+        if not class_name.isidentifier() or not class_name[0].isupper():
+            self.addWarning('InvalidName', node.lineno, f'invalid class name {class_name}')
+        self.generic_visit(node)  # Continuar la visita
+        self.current_class = None  # Restablecer la clase actual
+
+    def visit_FunctionDef(self, node):
+        if self.current_class:
+            # La función es un método de la clase actual
+            method_name = node.name
+            if not method_name.isidentifier() or not method_name[0].islower():
+                self.addWarning('InvalidName', node.lineno, f'invalid method name {method_name}')
+        else:
+            # La función no es un método de una clase
+            function_name = node.name
+            if not function_name.isidentifier() or not function_name[0].islower():
+                self.addWarning('InvalidName', node.lineno, f'invalid function name {function_name}')
+        self.generic_visit(node)
+
 
 
 class InvalidNameRule(Rule):
     def analyze(self, ast):
-        pass
+        visitor = InvalidNameVisitor()
+        visitor.visit(ast)
+        return visitor.warnings
     
     @classmethod
     def name(cls):
