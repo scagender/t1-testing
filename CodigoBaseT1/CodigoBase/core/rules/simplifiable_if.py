1f493edd016e0dc400cf452ed81668d0b57c3803
scagender <jpverafortune@uc.cl>
2024-04-04 17:27:14 -0300

    Simplifiable_if listo y codigo inciial de unintialized_attributed que es lo ultimo

--- CodigoBaseT1/CodigoBase/core/rules/simplifiable_if.py
+++ CodigoBaseT1/CodigoBase/core/rules/simplifiable_if.py
@@ -23,17 +23,37 @@ class SimplifiableIfVisitor(WarningNodeVisitor):
             node.body[0].value.value != node.orelse[0].value.value):
             self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
 
+        elif (isinstance(node.test, ast.Compare) and
+                isinstance(node.body[0], ast.Return) and
+                isinstance(node.orelse[0], ast.Return) and
+                isinstance(node.body[0].value, ast.Constant) and
+                isinstance(node.orelse[0].value, ast.Constant) and
+                node.body[0].value.value != node.orelse[0].value.value):
+                self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
+
+        # Continue visiting child nodes
+        self.generic_visit(node)
+        
+
+    def visit_Module(self, node):
         
         # Check for simplifiable ternary if
-
-        if (isinstance(node.test, ast.Compare) and
-            isinstance(node.body[0], ast.Return) and
-            isinstance(node.orelse[0], ast.Return) and
-            isinstance(node.body[0].value, ast.Constant) and
-            isinstance(node.orelse[0].value, ast.Constant) and
-            node.body[0].value.value != node.orelse[0].value.value):
-            self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
         
+        if (## If terneario
+            isinstance(node.body[0], ast.Return) and
+            isinstance(node.body[0].value, ast.IfExp) and
+            isinstance(node.body[0].value.body, ast.Constant) and
+            isinstance(node.body[0].value.orelse, ast.Constant) and
+            node.body[0].value.body.value != node.body[0].value.orelse.value):
+            self.addWarning('SimplifiableIf', node.body[0].lineno, 'if statement can be replaced with a bool(test)')
+        
+        elif (isinstance(node.body[0], ast.Assign) and
+            isinstance(node.body[0].value, ast.IfExp) and
+            isinstance(node.body[0].value.body, ast.Constant) and
+            isinstance(node.body[0].value.orelse, ast.Constant) and
+            node.body[0].value.body.value != node.body[0].value.orelse.value):
+            self.addWarning('SimplifiableIf', node.body[0].lineno, 'if statement can be replaced with a bool(test)')
+
         # Continue visiting child nodes
         self.generic_visit(node)
 
@@ -41,6 +61,7 @@ class SimplifiableIfVisitor(WarningNodeVisitor):
 class SimplifiableIfRule(Rule):
     def analyze(self, ast):
         visitor = SimplifiableIfVisitor()
+        
         visitor.visit(ast)
         return visitor.warnings
 
--- CodigoBaseT1/CodigoBase/core/rules/uninitialized_attribute.py
+++ CodigoBaseT1/CodigoBase/core/rules/uninitialized_attribute.py
@@ -1,4 +1,5 @@
 from ..rule import *
+import ast
 
 # Clases que permiten detectar si algun atributo no fue inicializado.
 # A veces se usan algunos atributos que no estan inicializados y esto genera errores.
@@ -7,12 +8,33 @@ class UninitializedAttributeVisitor(WarningNodeVisitor):
 
     def __init__(self):
         super().__init__()
+        self.warnings = []
+        self.current_class = None  # Mantener un seguimiento de la clase actual
+
+    def visit_ClassDef(self, node):
+        self.current_class = node  # Establecer la clase actual
+        self.generic_visit(node)  # Continuar la visita
+        self.current_class = None  # Restablecer la clase actual
+
+    def visit_Assign(self, node):
+        if self.current_class:
+            # Verificar asignaciones dentro de la clase
+            for target in node.targets:
+                if isinstance(target, ast.Attribute) and isinstance(target.value, ast.Name) \
+                        and target.value.id == 'self':
+                    # Es una asignación a un atributo de la clase
+                    attr_name = target.attr
+                    self.addWarning('UninitializedAttribute', node.lineno,
+                                    f'{attr_name} attribute was not initialized!')
 
 
 class UninitializedAttributeRule(Rule):
 
     def analyze(self, ast):
-        pass
+        visitor = UninitializedAttributeVisitor()
+        visitor.visit(ast)
+        return visitor.warnings
+
 
     @classmethod
     def name(cls):
