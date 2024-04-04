1b57c12ec73296e898121f03c3ae2a5b5ad84608
scagender <jpverafortune@uc.cl>
2024-04-04 01:36:16 -0300

    unused argument

--- CodigoBaseT1/CodigoBase/core/rules/simplifiable_if.py
+++ CodigoBaseT1/CodigoBase/core/rules/simplifiable_if.py
@@ -1,5 +1,6 @@
 from ..rule import *
-
+import ast
+import pdb
 # Clases que permiten detectar el uso de if statements o if ternarios que 
 # se pueden simplificar por la condición o la condición negada.
 
@@ -7,11 +8,42 @@ class SimplifiableIfVisitor(WarningNodeVisitor):
 
     def __init__(self):
         super().__init__()
+        self.warnings = []
+
+        
+## CAMBIAR CONSTANT POR NAMECONSTANT
+    def visit_If(self, node):
+        # Check for simplifiable if statement
+        if (isinstance(node.test, ast.Compare) and
+            isinstance(node.body[0], ast.Assign) and
+            isinstance(node.orelse[0], ast.Assign) and
+            node.body[0].targets[0].id == node.orelse[0].targets[0].id and
+            isinstance(node.body[0].value, ast.Constant) and
+            isinstance(node.orelse[0].value, ast.Constant) and
+            node.body[0].value.value != node.orelse[0].value.value):
+            self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
+
+        
+        # Check for simplifiable ternary if
+
+        if (isinstance(node.test, ast.Compare) and
+            isinstance(node.body[0], ast.Return) and
+            isinstance(node.orelse[0], ast.Return) and
+            isinstance(node.body[0].value, ast.Constant) and
+            isinstance(node.orelse[0].value, ast.Constant) and
+            node.body[0].value.value != node.orelse[0].value.value):
+            self.addWarning('SimplifiableIf', node.lineno, 'if statement can be replaced with a bool(test)')
+        
+        # Continue visiting child nodes
+        self.generic_visit(node)
 
 
 class SimplifiableIfRule(Rule):
     def analyze(self, ast):
-        pass
+        visitor = SimplifiableIfVisitor()
+        visitor.visit(ast)
+        return visitor.warnings
+
     
     @classmethod
     def name(cls):
--- CodigoBaseT1/CodigoBase/core/rules/unused_argument.py
+++ CodigoBaseT1/CodigoBase/core/rules/unused_argument.py
@@ -1,4 +1,6 @@
 from ..rule import *
+import ast
+import pdb
 
 # Clases que permiten detectar si un argumento no fue usado
 
@@ -6,12 +8,55 @@ class UnusedArgumentVisitor(WarningNodeVisitor):
 
     def __init__(self):
         super().__init__()
+        self.warnings = []
+    
+    def visit_FunctionDef(self, node):
+        used_args = set()
+        # Recorre los nodos del cuerpo de la función
+        for statement in node.body:
+            used_args.update(self.get_used_args(statement))
+        # Encuentra los argumentos no utilizados
+        for arg in node.args.args:
+            if arg.arg not in used_args and arg.arg != 'self':
+                self.addWarning('UnusedArgument', node.lineno, f"{arg.arg} argument has not been used!")
+                break
+        self.generic_visit(node)
+
+    def get_used_args(self, node):
+        used_args = set()
+        args = set()
+        pdb.set_trace()
+        # Caso base: si el nodo es una llamada a función
+        if isinstance(node, ast.Assign):
+            # Recorre los argumentos de la llamada
+            for target in node.targets:
+                try: 
+                    if isinstance(target, ast.Attribute):
+                        used_args.add(node.value.id)
+                except:
+                    pass
+        elif isinstance(node, ast.Call) or isinstance(node, ast.FunctionDef):
+            # Recorre los argumentos de la llamada
+            for arg in node.args:
+                # Si es un nombre, lo agrega a los argumentos utilizados
+                if isinstance(arg, ast.Name):
+                    used_args.add(arg.id)
+                elif isinstance(node, ast.Return):
+                    used_args.add(node.value.id)
+        # Si es un nodo compuesto, recorre sus hijos
+        elif hasattr(node, 'body'):
+            for child_node in node.body:
+                used_args.update(self.get_used_args(child_node))
+
+        return used_args
 
 
 class UnusedArgumentRule(Rule):
 
     def analyze(self, ast):
-        pass
+        visitor = UnusedArgumentVisitor()
+        visitor.visit(ast)
+        return visitor.warnings
 
     @classmethod
     def name(cls):
