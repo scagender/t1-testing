b5d86cd7f016ae50a242747aedffb2f2e3ef9b1b
scagender <jpverafortune@uc.cl>
2024-04-06 14:17:24 -0300

    equals rewriter

--- CodigoBaseT1/CodigoBase/core/transformers/operator_equals_rewriter.py
+++ CodigoBaseT1/CodigoBase/core/transformers/operator_equals_rewriter.py
@@ -1,15 +1,22 @@
 from ast import *
 from core.rewriter import RewriterCommand
+import pdb
 
-# Clases que permiten transformar codigo que contiene x = x <operador_aritmetico_binario> z a x <operador_aritmetico_binario>= z.
+# Clases que permiten transformar código que contiene x = x <operador_aritmetico_binario> z a x <operador_aritmetico_binario>= z.
 
 class OperatorEqualsTransformer(NodeTransformer):
 
     def __init__(self):
         super().__init__()
 
+    def visit_Assign(self, node):
+        if isinstance(node.value, BinOp) and isinstance(node.value.op, (Add, Sub, Mult, Div, Mod, Pow, FloorDiv)):
+            return AugAssign(target=node.targets[0], op=node.value.op, value=node.value.right)
+        return node.value
+
 
 class OperatorEqualsCommand(RewriterCommand):
 
     def apply(self, ast):
-        pass
+        new_tree = fix_missing_locations(OperatorEqualsTransformer().visit(ast))
+        return new_tree
