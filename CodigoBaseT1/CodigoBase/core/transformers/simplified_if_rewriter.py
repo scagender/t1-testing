d271cbbb23a340af225cd0417babbaf6c277db31
ercaceres <ercaceres@uc.cl>
2024-04-08 15:31:38 -0400

    simplified if rewriter resumido

--- CodigoBaseT1/CodigoBase/core/transformers/simplified_if_rewriter.py
+++ CodigoBaseT1/CodigoBase/core/transformers/simplified_if_rewriter.py
@@ -4,35 +4,6 @@ from core.rewriter import RewriterCommand
 
 # Clases que permiten transformar if ternarios que puedan ser simplificados usando la condicion o su negacion.
 
-"""class SimplifiedIfTransformer(NodeTransformer):
-    def __init__(self):
-        super().__init__()
-
-    def visit_IfExp(self, node):
-        if isinstance(node.test, ast.Compare):
-            comparison = node.test
-            if len(comparison.ops) == 1 and isinstance(comparison.ops[0], ast.Gt):
-                if isinstance(comparison.left, ast.Name) and isinstance(comparison.comparators[0], ast.Name):
-                    if comparison.left.id == comparison.comparators[0].id:
-                        if isinstance(node.body, ast.Name):
-                            return ast.Name(id="True", ctx=ast.Load())
-                        elif isinstance(node.orelse, ast.Name):
-                            return ast.Name(id="False", ctx=ast.Load())
-                elif isinstance(comparison.left, ast.Name) and isinstance(comparison.comparators[0], ast.Constant):
-                    if comparison.comparators[0].value == 0:
-                        if isinstance(node.body, ast.Name):
-                            return comparison.left
-                        elif isinstance(node.orelse, ast.Name):
-                            return ast.UnaryOp(op=ast.Not(), operand=comparison.left)
-            elif len(comparison.ops) == 1 and isinstance(comparison.ops[0], ast.Eq):
-                if isinstance(comparison.left, ast.Name) and isinstance(comparison.comparators[0], ast.Name):
-                    if comparison.left.id == comparison.comparators[0].id:
-                        if isinstance(node.body, ast.Name):
-                            return comparison.left
-                        elif isinstance(node.orelse, ast.Name):
-                            return ast.UnaryOp(op=ast.Not(), operand=comparison.left)
-        return node """
-
 class SimplifiedIfTransformer(NodeTransformer):
     def visit_IfExp(self, node):
         if isinstance(node.test, ast.Compare) and len(node.test.ops) == 1:
